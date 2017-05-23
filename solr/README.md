040044447532a503f673fb0a374f3670b5161759
Jean Valverde <jeanvalverde@catalyst.net.nz>
2017-05-23 15:19:52 +1200

    Bye bye Solr, moved to official image + build, remove composer set-up as now included in the Apache image

--- solr/README.md
+++
@@ -1,3 +0,0 @@
-# Docker Alpine Solr 5 with Drupal conf
-
-* Used for https://github.com/Mogtofu33/docker-compose-drupal
--- solr/scripts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "[i] Starting Solr..."
-# Run solr ignoring run as root warning.
-/opt/solr/bin/solr -f
