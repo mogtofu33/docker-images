e8002ac19a63624885a0f2076a663efac35078a7
Jean Valverde <jeanvalverde@catalyst.net.nz>
2017-05-24 14:41:01 +1200

    Clean delete script to install composer

--- apache-php5/scripts/run.sh
+++ apache-php5/scripts/run.sh
@@ -5,9 +5,6 @@ if [ "$LOCAL_UID" != "" ]; then
   /scripts/change_uid_gid.sh apache:www-data $LOCAL_UID:$LOCAL_GID
 fi
 
-# Set-up third party tools if needed.
-/scripts/install_tools.sh
-
 # Apache yelling about pid on container restart.
 rm -rf /run/apache2
 mkdir -p /run/apache2
