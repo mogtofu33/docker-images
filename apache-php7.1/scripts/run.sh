a27da492ac1fcf203c15aee02e6e152769e9730b
Jean Valverde <jeanvalverde@catalyst.net.nz>
2017-08-02 15:45:29 +1200

    Fix php 7.1, add code sniffer for Drupal, fix run with docker.sock permission fix

--- apache-php7.1/scripts/run.sh
+++ apache-php7.1/scripts/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Fix access docker.sock for our dashboard.
+if [ -f /var/run/docker.sock ]; then
+  chown apache:www-data /var/run/docker.sock
+fi
+
 # Set uid/gid to fix data permissions.
 if [ "$LOCAL_UID" != "" ]; then
   /scripts/change_uid_gid.sh apache:www-data $LOCAL_UID:$LOCAL_GID
@@ -15,9 +20,15 @@ echo "[i] Starting Apache..."
 httpd -D FOREGROUND
 
 # Test if apache is running, if not send error.
+sleep 1s
 if ! pgrep -x "httpd" > /dev/null
 then
   echo "[i] Apache stopped..."
   tail -n 5 /var/log/apache2/error.log
-  tail -n 5 /var/log/apache2/ssl_error.log
+  if [ -f /var/log/apache2/ssl_error.log ]; then
+    tail -n 5 /var/log/apache2/ssl_error.log
+  fi
+  exit 0;
 fi
+
+echo "[i] Apache running!"
