bad3418afeb18789b5eaf172032704bac78e2841
Jean Valverde <moimog33@gmail.com>
2019-04-19 15:36:50 +1000

    Codecasts proper php packages, move links fix to script.

--- php/scripts/run.sh
+++ php/scripts/run.sh
@@ -3,6 +3,24 @@
 # Common commands for permissions fix.
 /scripts/fix_permissions.sh
 
+# Fix Php links if needed.
+if ! [ -f /usr/sbin/php-fpm ]; then
+  ln -s /usr/sbin/php-fpm7 /usr/sbin/php-fpm
+fi
+
+if ! [ -f /usr/bin/php ]; then
+  ln -s /usr/bin/php7 /usr/bin/php
+fi
+
+# Add composer folder links for cache.
+if ! [ -d /composer ]; then
+  ln -s /root/.composer/ /composer
+fi
+
+if ! [ -d /.composer ]; then
+  ln -s /root/.composer/ /.composer
+fi
+
 echo "[i] Starting Php-fpm..."
 
 exec /usr/sbin/php-fpm -F
