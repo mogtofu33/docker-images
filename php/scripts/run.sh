02490cbd00fcb3686a4234af54f27a9e45ef3698
Jean Valverde <moimog33@gmail.com>
2021-03-27 15:49:28 +0100

    fix(php): php 8 bin start

--- php/scripts/run.sh
+++ php/scripts/run.sh
@@ -3,6 +3,9 @@
 if ! [ -f /usr/bin/php ] && [ -f /usr/bin/php7 ]; then
   ln -s /usr/bin/php7 /usr/bin/php
 fi
+if ! [ -f /usr/bin/php ] && [ -f /usr/bin/php8 ]; then
+  ln -s /usr/bin/php8 /usr/bin/php
+fi
 
 # Common commands for permissions fix.
 /scripts/fix_permissions.sh
@@ -15,4 +18,7 @@ then
 elif [ -f /usr/sbin/php-fpm7 ]
 then
   exec /usr/sbin/php-fpm7 -F
+elif [ -f /usr/sbin/php-fpm8 ]
+then
+  exec /usr/sbin/php-fpm8 -F
 fi
